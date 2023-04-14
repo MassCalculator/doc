e0d65d8682169b391cd01a821971126c2866b161
jimmyhalimi <jimmyhalimi@users.noreply.github.com>
2023-04-14 17:06:41 +0000

    Update documentation

--- docs/masscalculator-core/devel/html/search/functions_8.js
+++ docs/masscalculator-core/devel/html/search/functions_8.js
@@ -3,5 +3,5 @@ var searchData=
   ['luaget_3c_20std_3a_3astring_20_3e_629',['LuaGet&lt; std::string &gt;',['../namespacemasscalculator_1_1base.html#a1bfa1091020f47062436729950160cfa',1,'masscalculator::base']]],
   ['luaget_3c_20std_3a_3astring_5fview_20_3e_630',['LuaGet&lt; std::string_view &gt;',['../namespacemasscalculator_1_1base.html#a563ae9a1ff1e5985820f8e8a2ab794be',1,'masscalculator::base']]],
   ['luascripthandler_631',['LuaScriptHandler',['../classmasscalculator_1_1base_1_1LuaScriptHandler.html#a11b6a41ea2f10df788c7cc488e459a29',1,'masscalculator::base::LuaScriptHandler']]],
-  ['luascripthandlertest_632',['LuaScriptHandlerTest',['../classmasscalculator_1_1base_1_1LuaScriptHandlerTest.html#a1a1fb8807d8efcb27adc010620983331',1,'masscalculator::base::LuaScriptHandlerTest']]]
+  ['luascripthandlertest_632',['LuaScriptHandlerTest',['../classmasscalculator__test_1_1base__test_1_1LuaScriptHandlerTest.html#a764be721c0353498708fb0bbc2f75e0c',1,'masscalculator_test::base_test::LuaScriptHandlerTest']]]
 ];
